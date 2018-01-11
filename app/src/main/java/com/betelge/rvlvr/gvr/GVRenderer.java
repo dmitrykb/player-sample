1cc2360291574ff2d13e81d15d9528fe57276901
jasminko <betelge@gmail.com>
2018-01-11 11:58:20 +0700

    Add missing texture name

--- app/src/main/java/com/betelge/rvlvr/gvr/GVRenderer.java
+++ app/src/main/java/com/betelge/rvlvr/gvr/GVRenderer.java
@@ -235,6 +235,11 @@ public class GVRenderer implements GvrView.StereoRenderer, DriftRenderer {
 
     private void createYUVConverter() {
 
+        // YUV texture
+        int[] texLoc = {0};
+        GLES20.glGenTextures(1, texLoc, 0);
+        yuvTextureName = texLoc[0];
+
         // Shaders for YUV converter
         yuy2ConverterProgram = GLES20.glCreateProgram();
         nv12ConverterProgram = GLES20.glCreateProgram();
